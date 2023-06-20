2c717cb888072f2566aa2ed3c76dd7105a9d77b4
AzumattDev <azumattgaming@gmail.com>
2023-06-20 03:13:46 -0400

    Fuck me...I do this sometimes.

--- Plugin.cs
+++ Plugin.cs
@@ -14,7 +14,7 @@ namespace SimpleAudioOptions
     public class SimpleAudioOptionsPlugin : BaseUnityPlugin
     {
         internal const string ModName = "SimpleAudioOptions";
-        internal const string ModVersion = "1.0.0";
+        internal const string ModVersion = "1.0.1";
         internal const string Author = "Azumatt";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
